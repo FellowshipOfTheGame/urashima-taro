783ce84492030160c8f49bbf34b85e821c27a74f
RodrigoLimaRFL <guico21@usp.br>
2021-10-16 15:49:35 -0300

    adicionado sistema de vida

---
+++ Assets/Scripts/Jogador/Vida.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour
+{
+    [SerializeField] int vidaMax;
+
+    // TODO: tornar vidaAtual private
+
+    public int vidaAtual;
+
+    BarraDeVida barraScript;
+
+    void Start()
+    {
+        GameObject hp;
+
+        hp = GameObject.Find("UI_Jogador/HP");
+        barraScript = hp.GetComponent<BarraDeVida>();
+
+        vidaAtual = vidaMax;
+        barraScript.DefinirVidaMax(vidaMax);
+    }
+
+    void Update()
+    {
+        // temp
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Dano(5);
+        }
+
+        barraScript.DefinirVida(vidaAtual);
+    }
+
+    void Dano(int _dano)
+    {
+        vidaAtual = Mathf.Clamp(vidaAtual-_dano, 0, vidaMax);
+
+        barraScript.DefinirVida(vidaAtual);
+
+        if (vidaAtual == 0)
+        {
+            // morte
+            Destroy(gameObject);
+        }
+    }
+}
