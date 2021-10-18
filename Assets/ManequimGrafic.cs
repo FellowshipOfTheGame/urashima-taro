56bdfa729d8677627b012ea0add162eb95a30713
Tyago Teoi <tyagoyuji@gmail.com>
2021-10-18 18:32:47 -0300

    Add basic zombie AI movement
    Uses the Pathfinder and A* PATH FINDER to calculate the minimun path
    between the player and the IA and move the IA close to the player.
    
    Some modifications using 3d box collider for the obstacles have been
    added for the path fider to work properly.
    
    A plane have been added for the path finder too.

---
+++ Assets/ManequimGrafic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ManequimGrafic : MonoBehaviour
+{
+    public AIPath aiPath;
+
+
+    void Update()
+    {
+        //faz a animacao de acordo com a velocidade
+        //aiPath.desiredVelocity.x / .y
+        
+    }
+}
