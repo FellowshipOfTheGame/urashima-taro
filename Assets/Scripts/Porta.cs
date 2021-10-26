655a4a7e23c0e18fef2e27ebf60c97943c4351c6
AleLocci <alelocci@ime.usp.br>
2021-10-26 16:10:53 -0300

    Programação do comportamento da porta.
    Implementei o comportamento da porta. Quando player pega a chave ele tem direito de abrir uma porta. Pra tanto, ele ym de ir em direção à porta até ficar numa certa distância dessa porta.

--- Assets/Scripts/Porta.cs
+++ Assets/Scripts/Porta.cs
@@ -6,7 +6,11 @@ public class Porta : MonoBehaviour
 {
 
     public GameObject porta;
-    
+    public GameObject jogador;
+    float distancia  = 0f;
+    public float raioPorta = 3f;
+    bool abrir = false;
+
 
     void Start()
     {
@@ -16,16 +20,24 @@ public class Porta : MonoBehaviour
     void Update()
     {
 
-        if (Flag.porta)
-        {
+      distancia = Vector3.Distance(jogador.transform.position, porta.transform.position);
 
-            float posiEmX = porta.transform.position.x;
-            float posiEmY = porta.transform.position.y;
+      if (Flag.porta && distancia < raioPorta)
+        {           
 
-            porta.transform.position = new Vector3(posiEmX, (posiEmY + 2), 0.0f);//definir qual o evento ocorrera sobre a porta.
-            Flag.porta = false;
+                float posiEmX = porta.transform.position.x;
+                float posiEmY = porta.transform.position.y;
+
+                porta.transform.position = new Vector3(posiEmX, (posiEmY + 2), 0.0f);//definir qual o evento ocorrera sobre a porta.
+                Flag.porta = false;                                 
 
         }
         
     }
 }
+/*
+ InvalidOperationException: You are trying to read Input using the UnityEngine.Input class, but you have switched active Input handling to Input System
+package in Player Settings.
+UnityEngine.Input.GetKey (UnityEngine.KeyCode key) (at <6af207ecd21044628913f7cc589986ae>:0)
+Porta.Update () (at Assets/Scripts/Porta.cs:31)
+ */
