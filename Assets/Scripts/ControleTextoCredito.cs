bd73fba13ee4f83cd39e4e4ce40337d4dc54a116
AleLocci <alelocci@ime.usp.br>
2022-07-24 09:13:31 -0300

    inserido texto.

--- Assets/Scripts/ControleTextoCredito.cs
+++ Assets/Scripts/ControleTextoCredito.cs
@@ -6,29 +6,38 @@ using UnityEngine.UI;
 public class ControleTextoCredito : MonoBehaviour
 {
     int num;
+    int index;
+    string[] nomes = {"nome1","nome2","nome3","nome4","nome5","nome6"};
     public Text credito1;
+    string str;
+    const int MAXIMORANGE  = 1000000;
     // Start is called before the first frame update
     void Start()
     {
         credito1.text = "";
+        index = 0;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-        num = Random.Range(0, 200);
-        
-        if(num < 100)
-        {
-        
-             credito1.text = num.ToString();
-        
-        }
-        
-       
+    {             
         
         
+        num = Random.Range(0, MAXIMORANGE);
         
-    }
+         Debug.Log(num);       
+        if(5 < num  && index < nomes.Length)
+        { 
+        
+            str = str + nomes[index] + "\n";
+            //str = nomes[index] + "\n";
+            credito1.text = str;
+            index ++;
+        
+        }     
+               
+        
+    }    
+    
+    
 }
