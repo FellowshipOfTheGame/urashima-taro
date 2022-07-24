8bca22b8b86d60e08e2694bcb5f9d33fe577d2f5
AleLocci <alelocci@ime.usp.br>
2022-07-24 07:58:58 -0300

    criacao cena de abertura.

--- Assets/Scripts/ControleTextoCredito.cs
+++ Assets/Scripts/ControleTextoCredito.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControleTextoCredito : MonoBehaviour
 {
+    int num;
+    public Text credito1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        credito1.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        num = Random.Range(0, 200);
+        
+        if(num < 100)
+        {
+        
+             credito1.text = num.ToString();
+        
+        }
+        
+       
+        
+        
         
     }
 }
