eb256b6613ef57709c4acc6268c4740c812f9374
Tyago-Teoi <tyago.teoi@usp.br>
2022-09-18 17:56:08 -0300

    Update DevPreload script to add initial position to player

--- Assets/Scripts/_preload/DevPreload.cs
+++ Assets/Scripts/_preload/DevPreload.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 // Used for access the preload scene from any scene and other stuffs
 // This is a Singleton
 // obs: ONLY USED FOR DEVELOPMENT, NOT IMPLEMENTED FOR THE GAME
 public class DevPreload : MonoBehaviour
 {
     public static DevPreload Instance { get; private set; }
-    public enum Type {_preload, Menu, Test, Teoi, HerbertH, HerbertH2, HerbertH3, TesteHerbert, house1Floor , house2Floor };
+    public enum Type {_preload, Menu, Test, Teoi, HerbertH, HerbertH2, HerbertH3, TesteHerbert, ZombieTest };
     public Type nextScene;
+    [SerializeField] private Vector3 destinyPosition;
 
     void Awake()
     {
@@ -29,6 +31,13 @@ public class DevPreload : MonoBehaviour
         if (check == null)
         {
             SceneManager.LoadScene("_preload");
+        }
+
+        if (destinyPosition != null)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+
+            player.transform.position = destinyPosition;
         }
     }
 }
