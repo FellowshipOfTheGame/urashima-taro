7dab0526641d0b25352ea4891f87e51aa99d6ba8
Tyago Yuji Teoi <tyago.teoi@usp.br>
2022-10-16 18:32:52 -0300

    Fix pause menu volume menu mixer bugs

--- Assets/Scripts/PauseMenu/SettingsMenu.cs
+++ Assets/Scripts/PauseMenu/SettingsMenu.cs
@@ -5,28 +5,28 @@ public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
-    private void Start()
+    private void Awake()
     {
-        SetVolume(PlayerPrefs.GetFloat("main-volume", 0f));
-        SetMusicVolume(PlayerPrefs.GetFloat("music-volume", 0f));
-        SetEffectsVolume(PlayerPrefs.GetFloat("effects-volume", 0f));
+        SetVolume(PlayerPrefs.GetFloat("main-volume", 0));
+        SetMusicVolume(PlayerPrefs.GetFloat("music-volume", 0));
+        SetEffectsVolume(PlayerPrefs.GetFloat("effects-volume", 0));
     }
 
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("main-volume", volume);
+        audioMixer.SetFloat("main-volume", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("main-volume", volume);
     }
 
     public void SetMusicVolume(float volume)
     {
-        audioMixer.SetFloat("music-volume", volume);
+        audioMixer.SetFloat("music-volume", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("music-volume", volume);
     }
 
     public void SetEffectsVolume(float volume)
     {
-        audioMixer.SetFloat("effects-volume", volume);
+        audioMixer.SetFloat("effects-volume", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("effects-volume", volume);
     }
 }
--- Assets/Scripts/PauseMenu/VolumePanel.cs
+++ Assets/Scripts/PauseMenu/VolumePanel.cs
@@ -5,17 +5,16 @@ public class VolumePanel : MonoBehaviour
 {
     [SerializeField] private Slider masterVolumeSlider, effectsVolumeSlider, musicVolumeSlider;
 
-    private void Awake()
+    private void Start()
     {
         var audioMixer = FindObjectOfType<SettingsMenu>().audioMixer;
 
         audioMixer.GetFloat("main-volume", out var masterValue);
-        masterVolumeSlider.value = masterValue;
-
+        masterVolumeSlider.value = Mathf.Pow(10, masterValue / 20);
         audioMixer.GetFloat("effects-volume", out var effectsValue);
-        effectsVolumeSlider.value = effectsValue;
+        effectsVolumeSlider.value = Mathf.Pow(10, effectsValue / 20);
 
         audioMixer.GetFloat("music-volume", out var musicValue);
-        musicVolumeSlider.value = musicValue;
+        musicVolumeSlider.value = Mathf.Pow(10, musicValue / 20);
     }
 }
