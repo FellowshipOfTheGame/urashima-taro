bcb6f89b5334be1310b667f15f542eeb44562055
Tyago Teoi <tyago.teoi@usp.br>
2022-10-24 16:47:23 -0300

    Add WeaponBadge HUD to the game
    Adds the weapon icon related to the weapon that
    the player is using, and the number os uses.

--- Assets/Scripts/Inventory/Buttons/WearWeapon.cs
+++ Assets/Scripts/Inventory/Buttons/WearWeapon.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class WearWeapon: MonoBehaviour
 {
+    [SerializeField] private Image _weaponImage;
+    [SerializeField] private TextMeshProUGUI _bulletNumber;
+
     private GameObject player;
     private static GameObject weaponPrefab;
     [HideInInspector] public ItemSO item;
@@ -20,6 +25,12 @@ public class WearWeapon: MonoBehaviour
         {
             Destroy(weaponPrefab);
         }
+
+        // Changes the WeaponBadge's icon and bullet number
+        _weaponImage.sprite = item.icon;
+        _bulletNumber.text = item.quantity.ToString();
+
+
         // Instantiate the prefab containing the weapon script
         weaponPrefab = Instantiate(item.itemPrefab, player.transform.position, Quaternion.identity);
         // Sets the prefab parent to be the player object
